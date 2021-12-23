04fac433947e6f84951e69c239e779a47137a36b
wanghao <muyao1987@qq.com>
2021-12-23 15:35:31 +0800

    删除dist

--- dist/example/layer-tile/online/ion/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <interest-search :map-work="mapWork" />
-</template>
-
-<script setup lang="ts">
-import InterestSearch from "@comp/mars-sample/interest-search.vue"
-import * as mapWork from "./map.js"
-</script>
